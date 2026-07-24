2de0b5090688ee812a07a23ebb6f8e3cd41cb468
leyurbano <leyurbano@gmail.com>
2026-07-24 18:11:14 -0500

    adding styles and modifying the logo

--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -7,15 +7,14 @@ function InicioPage() {
 
   return (
     <div className="welcome-container">
-         <img src="/Logo.png" alt="Fralu" className="welcome-logo" />
+         
       <div className="welcome-card">
-       
-        <h1 className="welcome-titulo">Tiquet-App</h1>
-        <p className="welcome-subtitulo">Sistema de punto de venta</p>
-        <button className="welcome-boton" onClick={() => navigate('/login')}>
+       <img src="/logoApp2.png" alt="Tiquet-App" className="welcome-logo" />
+           <button className="welcome-boton" onClick={() => navigate('/login')}>
           Ingresar
         </button>
       </div>
+     
     </div>
   );
 }
